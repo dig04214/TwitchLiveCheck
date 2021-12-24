85b7a00d7cd29888405e954850c0e0501e0f29e1
dig04214 <dig04214@naver.com>
2021-12-25 06:00:07 +0900

    python 파일 생성

---
+++ TwitchRecorder.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import time
+import json
+import sys
+import subprocess
+import datetime
+import getopt
+
+
+class TwitchRecorder:
+  def __init__(self) -> None:
+      pass
+
+
+
+
+def main(argv):
+  twitchrecorder = TwitchRecorder()
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
