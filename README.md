1eb4b3d760b93a8c44b3d91da021758cd7b49d4f
dig04214 <dig04214@naver.com>
2021-12-31 08:27:36 +0900

    화질 탐색 반복 기능 추가, Exception handler 코드 간소화, 마크다운 최신화

--- README.md
+++ README.md
@@ -7,11 +7,11 @@
 구현 완료   
 * 다수의 스트리머 생방송 여부 확인
 * 화질 설정
+* 화질 탐색 반복 기능(생방송 초기에 저화질만 나오는 경우 일정 횟수 동안 원하는 화질이 생겼는지 확인)
 * 영상 저장 위치 지정
 * streamlink를 활용해 생방송 다운로드
 
 미구현 기능   
-* 화질 탐색 반복 기능
 * argv 기능
 
 ## requirement:
@@ -22,6 +22,7 @@
 
 
 ## 사용방법:
-1. TwitchLiveCheck.py을 에디터로 열기
-2. __init(self)__ 부분에서 self로 시작하는 변수에 원하는 값 입력 후 저장
-3. 실행
+1. https://dev.twitch.tv/console/apps/create 에 접속해 응용 프로그램 등록 후 client id, client secret 생성
+2. TwitchLiveCheck.py를 에디터로 열기
+3. 주석 참고해 init 내부 변수 수정(현재는 별도의 configKey.py을 생성해야 함 -> 추후 수정)
+4. 실행
--- TwitchLiveCheck.py
+++ TwitchLiveCheck.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import time
-# import json
 import sys
 import subprocess
 import datetime
@@ -13,11 +12,10 @@ import configKey
 class TwitchLiveCheck:
   def __init__(self) -> None:
     self.streamerID = configKey.streamerID   # 스트리머 ID, 띄어쓰기로 구분
-    self.quality = "best"   # 화질 설정, 1080p60, 1080p, best 중 하나 추천
-    self.refresh = 1.5   # 탐색 간격(초) 설정, 0.5이하의 값 금지
+    self.quality = "1080p60"   # 화질 설정, 1080p60, 1080p, best 중 하나 추천
+    self.refresh = 1.0   # 탐색 간격(초) 설정, 0.5이하의 값 금지
     self.check = 30   # 화질 탐색 횟수 설정, 탐색 횟수 이상으로 설정된 화질 없으면 best로 바꿈
     self.root_path = configKey.root_path   # 저장 경로 설정
-
 
     self.client_id = configKey.client_id   # client ID 설정, twitch developers에서 발급
     self.client_secret = configKey.client_secret   # client secret 설정, twitch developers에서 발급
@@ -30,6 +28,8 @@ class TwitchLiveCheck:
     self.procs = {}
     self.stream_quality = dict.fromkeys(self.login_name, self.quality)
     self.check_num = dict.fromkeys(self.login_name, 0)
+    atexit.register(self.terminate_proc)
+    atexit.register(self.revoke_token)
     for id in self.login_name:
       self.download_path[id] = os.path.join(self.root_path, id)
       if(os.path.isdir(self.download_path[id]) is False):
@@ -73,9 +73,7 @@ class TwitchLiveCheck:
     for id in username:
       params = params + f'&user_login={id}'
     params = params[1:]
-
     return params
-
 
   def check_live(self) -> tuple:
     self.validate_token()
@@ -83,58 +81,65 @@ class TwitchLiveCheck:
     h = {'Authorization': f'Bearer {self.user_token}', 'Client-Id': self.client_id}
     info = {}
     status = False
-    try:
-      if self.login_name != []:
-        res = requests.get(api, headers=h)
-        res.raise_for_status()
-        if len(res.json()['data']) == 0:
-          status, info = False, {}
-        else:
-          for i in res.json()['data']:
-            info[i['user_login']] = {'title': i['title'], 'game': i['game_name']}
-            self.login_name.remove(i['user_login'])
-          self.url_params = self.create_params(self.login_name)
-          status = True
+    if self.login_name != []:
+      res = requests.get(api, headers=h)
+      res.raise_for_status()
+      if len(res.json()['data']) == 0:
+        status, info = False, {}
+      else:
+        for i in res.json()['data']:
+          info[i['user_login']] = {'title': i['title'], 'game': i['game_name']}
+          self.login_name.remove(i['user_login'])
+        self.url_params = self.create_params(self.login_name)
+        status = True
+    return status, info
 
+  def loop_check(self) -> None:
+    try:
+      while True:
+        status, info = self.check_live()
+        if status is True:
+          for id in info:
+            print(id, 'is online. Stream recording in session.')
+            title = info[id]['title'] if info[id]['title'] != '' else 'Untitled'
+            game = info[id]['game'] if info[id]['game'] != '' else 'Unknown'
+            filename = id + ' - ' + datetime.datetime.now().strftime("%Y%m%d %Hh%Mm%Ss") + '_' + title + '_' + game + '.ts'
+            filename = "".join(x for x in filename if x.isalnum() or x not in ['\\', '/', ':', '*', '?', '\"', '<', '>', '|'])
+            file_path = os.path.join(self.download_path[id], filename)
+            self.procs[id] = subprocess.Popen(['streamlink', "--stream-segment-threads", "5", "--stream-segment-attempts" , "5", "--twitch-disable-hosting", "--twitch-disable-ads", 'www.twitch.tv/' + id, self.stream_quality[id], "-o", file_path])
+        if self.login_name != []:
+          print('', self.login_name, 'is offline. Check again in', self.refresh, 'seconds.')
+        self.check_process()
+        time.sleep(self.refresh)
     except (KeyboardInterrupt, SystemExit):
       self.terminate_proc()
       self.revoke_token()
       raise
 
-    return status, info
-
-  def loop_check(self) -> None:
-    while True:
-      status, info = self.check_live()
-      if status is True:
-        for id in info:
-          print(id, 'is online. Stream recording in session.')
-          title = info[id]['title'] if info[id]['title'] != '' else 'Untitled'
-          game = info[id]['game'] if info[id]['game'] != '' else 'Unknown'
-          filename = id + ' - ' + datetime.datetime.now().strftime("%Y%m%d %Hh%Mm%Ss") + '_' + title + '_' + game + '.ts'
-          filename = "".join(x for x in filename if x.isalnum() or x not in ['\\', '/', ':', '*', '?', '\"', '<', '>', '|'])
-          file_path = os.path.join(self.download_path[id], filename)
-          self.procs[id] = subprocess.Popen(['streamlink', "--stream-segment-threads", "5", "--stream-segment-attempts" , "5", "--twitch-disable-hosting", "--twitch-disable-ads", 'www.twitch.tv/' + id, self.stream_quality[id], "-o", file_path])
-        # 화질 체크 반복 기능 필요
-      if self.login_name != []:
-        print(self.login_name, 'is offline. Check again in', self.refresh, 'seconds.')
-      self.check_process()
-      time.sleep(self.refresh)
-
   def check_process(self) -> None:
     id_status = False
-    if len(self.procs) > 0:
-      for id in self.procs:
+    if self.procs != {}:
+      for id in list(self.procs.keys()):
         proc_code = self.procs[id].poll()
         if proc_code == None:
-          # print(id, " is still recording")
+          # 실행 중
           pass
         elif proc_code == 0:
-          # print(id, " is still recording")
-          pass
-        else:
+          # 정상 종료
           print(id, "stream is done. Go back checking...")
+          del self.procs[id]
           self.login_name.append(id)
+          id_status = True
+        else:
+          # 비정상 종료. ex 화질 없음
+          self.check_num[id] += 1
+          print(id, "stream error. Check:", self.check_num[id])
+        if self.check_num[id] >= self.check:
+          self.stream_quality[id] = 'best'
+          print('Change', id, 'stream quality to best' )
+          del self.procs[id]
+          self.login_name.append(id)
+          self.check_num[id] = 0
           id_status = True
     if id_status is True:
       self.url_params = self.create_params(self.login_name)
@@ -144,18 +149,11 @@ class TwitchLiveCheck:
     if len(self.procs) > 0:
       for id in self.procs:
         self.procs[id].terminate()
+        self.procs[id].poll()
 
-  def __del__(self) -> None:
-    self.terminate_proc()
-    self.revoke_token()
-    
 def main(argv):
   twitch_check = TwitchLiveCheck()
-
   twitch_check.run()
-  atexit.register(twitch_check.revoke_token())
-  atexit.register(twitch_check.terminate_proc())
-
 
 if __name__ == '__main__':
   main(sys.argv[1:])
